bc964a7fb5ab238dba48587ad5096cd06972cf10
Mac <mac@abctech-thailand.com>
2016-04-01 16:12:56 +0700

    Create cattail endpoint

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var spawn = require('child_process').spawn;
+
+var app = express();
+var portNumber = 8080;
+
+var spawnTail;
+app.get('/cattail', function(req, res) {
+
+  spawnTail = spawn('tail', ['-f', 'PATH']);
+  res.header('Content-Type','text/html;charset=utf-8');
+
+  spawnTail.stdout.on('data', function(data) {
+    res.write(data.toString().split("\n").join("<br />"), 'utf-8');
+  });
+
+  spawnTail.on('exit', function(code) {
+    res.end(code);
+  });
+
+});
+
+app.listen(portNumber, function () {
+  console.log('App listening on port ' + portNumber);
+});
